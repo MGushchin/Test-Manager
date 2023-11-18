dc656b8ef21cba8723aff3dbafd3117fb22858d5
MGushchin <uevfrc@gmail.com>
2023-11-18 19:06:43 +0700

    Styles added
    Added CSS styles for pages, fixed minor bugs

--- Test Manager Alpha/Test Manager Alpha/Program.cs
+++ Test Manager Alpha/Test Manager Alpha/Program.cs
@@ -12,6 +12,8 @@ builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
+app.UseStaticFiles();
+
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
